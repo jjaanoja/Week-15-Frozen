c6ed88604b2f27c6d4d9633ee692d0745bf2c712
Janno Jaanoja <jjaanoja@gmail.ee>
2022-03-16 12:29:05 +0200

    new projekt

---
+++ Frozen/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Frozen
+{
+    class Program
+    {
+        class SecretSanta
+        {
+            string name;
+            string gift;
+
+            public SecretSanta(string _name, string _gift)
+            {
+                name = _name;
+                gift = _gift;
+            }
+
+           
+
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+            }
+
+            public string Gift
+            {
+                get
+                {
+                    return gift;
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            List<SecretSanta> mySecretSantas = new List<SecretSanta>();
+            string[] secretsantasFromFile = GetDataFromFile();
+
+            foreach (string line in secretsantasFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                SecretSanta newSecretSanta = new SecretSanta(tempArray[0], tempArray[1]);
+                mySecretSantas.Add(newSecretSanta);
+
+            }
+
+            foreach (SecretSanta secretSantaFromList in mySecretSantas)
+            {
+                Console.WriteLine($"{secretSantaFromList.Name} wants {secretSantaFromList.Gift}.");
+            }
+
+        }
+
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach (string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+
+
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\Janno\Samples\frozen.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+
+
+    }
+
+}
